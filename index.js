797ee27abf4add09a7839bb9da62000613cb9875
parsa <parsaddbb@gmail.com>
2024-04-14 18:59:50 +0330

    start backend

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import Router from "./src/index.js";
+import dotenv from "dotenv";
+dotenv.config();
+import sequelize from "./utiles/db.js";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use("/api", Router);
+
+const PORT = process.env.PORT || 3000;
+sequelize.sync();
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
+});
