646190f248d145707dd7b2b10719a34b47691671
parsa <parsaddbb@gmail.com>
2024-04-21 21:05:41 +0330

    manage register saved email

--- src/routes/auth/controller.js
+++ src/routes/auth/controller.js
@@ -1,16 +1,25 @@
 import controller from "../../controller.js";
 export default new (class extends controller {
   async register(req, res) {
+    const user = await this.User.findOne({ where: { email: req.body.email } });
+    if (user)
+      return this.response({
+        code: 400,
+        message: "این ایمیل قبلا ثبت شده است",
+        res,
+        data: {},
+      });
     const data = await this.User.create(req.body);
     if (!data)
       return this.response({
-        status: 400,
+        code: 400,
         message: "عملیات ناموفق",
         res,
         data: data,
       });
-    this.response({
-      status: 200,
+
+    return this.response({
+      code: 200,
       message: "کاربر با موفقیت ثبت نام شد",
       res,
       data: data,
@@ -26,25 +35,24 @@ export default new (class extends controller {
     console.log(user);
     if (!user)
       return this.response({
-        status: 400,
+        code: 400,
         message: "برای ورورد ابتدا ثبت نام کنید",
         res,
         data: {},
       });
-
+    console.log(user.dataValues.password);
     if (user.dataValues.password != password)
       return this.response({
-        status: 400,
+        code: 400,
         message: "ایمیل یا رمز عبور اشتباه است",
         res,
         data: user,
       });
     return this.response({
-      status: 200,
+      code: 200,
       message: "کاربر گرامی با موفقیت وارد شدید",
       res,
       data: user,
     });
-    // console.log(user);
   }
 })();
