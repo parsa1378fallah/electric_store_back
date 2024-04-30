05c3c21b7572b8dad85dc57460c0c124139acef0
parsa <parsaddbb@gmail.com>
2024-04-30 22:50:58 +0330

    create upload profile image

--- src/routes/upload/controller.js
+++ src/routes/upload/controller.js
@@ -1,13 +1,20 @@
 import controller from "../../controller.js";
 export default new (class extends controller {
   async uploadProfileImage(req, res) {
-    const { image } = req.body;
-    console.log(req.body);
+    const { userId } = req.params;
+    let user = await this.User.findOne({ userId: userId });
+    user.profileImage = `http://localhost:3000/${req.file.filename}`;
+    user = await user.save();
+    console.log(user);
     return this.response({
       code: 200,
       res,
       message: "این یک تست است",
-      data: image,
+      data: {
+        filename: req.file.filename,
+        url: `http://localhost:3000/${req.file.filename}`,
+        user,
+      },
     });
   }
 })();
--- src/routes/upload/index.js
+++ src/routes/upload/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import controller from "./controller.js";
-
+import { upload } from "../../middlewares/uploadImage.js";
 const router = express.Router();
 
-router.post("/profile-image", controller.uploadProfileImage);
+router.post(
+  "/profile-image/:userId",
+  upload.single("file"),
+  controller.uploadProfileImage
+);
 export default router;
